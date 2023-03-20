7883e2875b67a4124c6f959b12da520d4d645192
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-03-20 11:42:58 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// TODO: Add a comment giving a brief description of what React is
+// React is for single page app and enalbes dynamic component change.
+// ?? HOW DOES THIS IMPORT WORK??? 
+import React from 'react';
+
+// TODO: Add a comment that describes the difference between react and react-dom
+// React --> User Interface // ReactDOM --> Allows React to interact with DOM 
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+// TODO: Add a comment describing the significance of the ReactDOM.render method
+// 
+ReactDOM.render(<App />, document.getElementById('root'));
